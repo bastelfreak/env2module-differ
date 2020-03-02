23ca72825c797eaa302951b28a0fb24df951c59f
Tim Meusel <tim@bastelfreak.de>
2020-03-03 00:12:10 +0100

    restructure all the things

--- main.rb
+++ main.rb
@@ -129,6 +129,39 @@ def load_cache(cachedir)
   YAML.safe_load(File.open("#{cachedir}/cache.yaml"))
 end
 
+# $1 is the PuppetDB client
+def all_nodes_with_os(client)
+  response = client.request('facts', [:'=', 'name', 'operatingsystem'])
+  response.data
+end
+
+# $1 is the PuppetDB client
+def all_nodes_with_os_major_version(client)
+  response = client.request('facts', [:'=', 'name', 'operatingsystemmajrelease'])
+  response.data
+end
+
+# $1 is an array of hashes with certname + OS
+# $2 is an array of hashes with certname + major OS version
+def nodes_with_os_and_version(nodes_with_operatingsystem, nodes_with_operatingsystemmajrelease)
+  nodes_with_os_and_version = {}
+  nodes_with_operatingsystem.each do |server|
+    os = server['value']
+    # get the operating system major version for the current server
+    # `find` returns an hash. We only want the `value` key
+    os_version = nodes_with_operatingsystemmajrelease.find { |node| node['certname'] == server['certname'] }['value']
+    # naming schema is `OS`-`Majorversion`
+    # Except for rolling releases Like Arch and Gentoo
+    rolling = %w[Archlinux Gentoo]
+    nodes_with_os_and_version[server['certname']] = if rolling.include?(server['value'])
+                                                      os
+                                                    else
+                                                      "#{os}-#{os_version}"
+                                                    end
+  end
+  nodes_with_os_and_version
+end
+
 def modules_metadata(path)
   metadatas = {}
   Dir.glob("#{path}/*/metadata.json") do |metadata|
@@ -144,12 +177,18 @@ def modules_metadata(path)
   metadatas
 end
 
+# This currently doesn't parse OS dependencies correctly if they don't have an array
+# That's sometimes the case for rolling release distros like Arch and Fedora
 def generate_os_version_names(metadatas)
-  metadatas.map do |_puppetmodule, metadata|
+  metadatas.map do |_module, metadata|
     os_version_names = if metadata['operatingsystem_support']
                          data = metadata['operatingsystem_support'].map do |os|
                            if os['operatingsystemrelease'].nil?
+                             if ['Gentoo', 'Archlinux'].include?(os['operatingsystem'])
+                               [os['operatingsystem']]
+                             else
                              []
+                             end
                            else
                              os['operatingsystemrelease'].map { |rel| "#{os['operatingsystem']}-#{rel}" }
                            end
@@ -163,23 +202,68 @@ def generate_os_version_names(metadatas)
   metadatas
 end
 
+# We wan't to have links to the repos in the markdown table
+def get_all_modules_with_links(all_modules, metadatas)
+  all_modules.map do |modul|
+    name = metadatas[modul]['name'].nil? ? name : metadatas[modul]['name']
+    if metadatas[modul]['project_page'].nil?
+      name
+    else
+      "[#{name}](#{metadatas[modul]['project_page']})"
+    end
+  end
+end
+
+def all_used_modules(nodes_with_os_and_version)
+  final_data = {}
+  nodes_with_os_and_version.each do |certname, os|
+    modules = all_used_modules_on_one_server(certname)
+    puts "processed: #{certname} with #{modules.count} modules on #{os}"
+    puts modules.join(', ')
+
+    # ensure that our final array contains a hash for that OS-Version combination
+    final_data[os] = [] unless final_data[os]
+    final_data[os] = (final_data[os] + modules)
+  end
+end
+
+# $1 is the common name from a valid agent certificate
+# $2 is the PuppetDB client object
+def all_used_modules_on_one_server(certname, client)
+  # get a catalog for this common name
+  catalog = client.request('catalogs', [:"=", 'certname', certname])
+
+  # get all classes
+  resources = catalog.data.first['resources']['data']
+  # requires ruby2.7
+  # modules = resources.filter_map{|data| data['title'].split('::')[0] if data['type'] == 'Class'}
+
+  # Get all resources that are classes
+  classes = resources.filter { |data| data['type'] == 'Class' }
+  # Get all top level modules, ignore subclasses
+  modules = classes.map { |data| data['title'].split('::')[0].downcase }.uniq.sort
+
+  # remove classes that aren't modules
+  modules -= %w[main settings]
+end
+
 # $1 = A Hash. Each key is an OS, Each value an array of used modules on that OS
 # $2 = an array with all used modules (NOT all modules in the environment)
 # $3 = all metadatas in a hash, extended
 def render_master_markdown(os_module_hash, all_modules, metadatas)
   new_data = []
-  new_data << all_modules
+  new_data << get_all_modules_with_links(all_modules, metadatas)
   os_module_hash.each do |os, moduls_on_os|
     new_column = []
     all_modules.each do |modul_in_env|
       new_column << if moduls_on_os.include?(modul_in_env)
                       if metadatas[modul_in_env]['os_version_names'].include?(os)
-                        'used, os in metadata'
+                        'used'
                       else
-                        'used, os not in metadata'
+                        'incomplete'
                       end
                     else
-                      'module not used'
+                      'not used'
                     end
     end
     new_data << new_column
@@ -187,9 +271,15 @@ def render_master_markdown(os_module_hash, all_modules, metadatas)
   new_data
 end
 
-# to generate the full table
+# to generate the full table (only with used modules, not *all* modules in an env)
 # metadatas = modules_metadata('/home/bastelfreak/env2module-differ/modules')
 # metadatas_enhanced = generate_os_version_names(metadatas)
+# homedir = Dir.home
+# cachedir = "#{homedir}/.cache/env2module-differ"
+# os_module_hash = load_cache(cachedir)
+# labels = final_data.keys
+# labels = ['Modules \ OS'] + labels
+# all_modules = final_data.map { |os| os[1] }.flatten.sort.uniq
 # data = render_master_markdown(os_module_hash, all_modules, metadatas_enhanced)
 # table = MarkdownTables.make_table(labels, data)
 # File.open('module_os_matrix_complete.md', 'w') { |file| file.write(table) }
